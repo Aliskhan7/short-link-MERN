6912ccbb7d3e72b36e4ef56eaaa07764ce5b4ed6
aliskhangazamatov <ag@farcana.com>
2024-05-21 01:56:46 +0300

    create linkShort

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -1,24 +1,49 @@
 const {Router} = require('express')
 const Link = require('../models/Link')
+const shortid = require('shortid')
 const router = Router()
+const auth =require('../middleware/auth.middleware')
+const config = require('config')
 
-router.post('/generate', async (req, res) => {
+router.post('/generate', auth, async (req, res) => {
   try{
+    const baseUrl = config.get('baseURL')
+    const {from} = req.body
+
+    const code = shortid.generate()
+
+    const existing = await Link.findOne({from})
+
+    if(existing){
+      return res.json({link: existing})
+    }
+
+    const to = baseUrl + '/to/' + code
+
+    const link = new Link({
+      code, from, to, owner: req.user.userId
+    })
+
+    await link.save()
+
+    res.status(201).json({link})
 
   }catch (e) {
     res.status(500).json({message: 'Error, try again...('})
   }
 })
-router.get('/', async (req, res) => {
+router.get('/', auth, async (req, res) => {
   try{
-
+    const links = await Link.find({owner: req.user.userId})
+    res.json(links)
   }catch (e) {
     res.status(500).json({message: 'Error, try again...('})
   }
 })
-router.get('/:id', async (req, res) => {
+router.get('/:id', auth, async (req, res) => {
   try{
-
+    const link = await Link.findById(req.params.id)
+    res.json(link)
   }catch (e) {
     res.status(500).json({message: 'Error, try again...('})
   }
