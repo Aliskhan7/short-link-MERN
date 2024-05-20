0a916229be6c74f1c304b0cac392a107189a73ff
aliskhangazamatov <ag@farcana.com>
2024-05-20 23:41:39 +0300

    fix

---
+++ routes/link.routes.js
@@ -0,0 +1,27 @@
+const {Router} = require('express')
+const Link = require('../models/Link')
+const router = Router()
+
+router.post('/generate', async (req, res) => {
+  try{
+
+  }catch (e) {
+    res.status(500).json({message: 'Error, try again...('})
+  }
+})
+router.get('/', async (req, res) => {
+  try{
+
+  }catch (e) {
+    res.status(500).json({message: 'Error, try again...('})
+  }
+})
+router.get('/:id', async (req, res) => {
+  try{
+
+  }catch (e) {
+    res.status(500).json({message: 'Error, try again...('})
+  }
+})
+
+module.exports = router()
